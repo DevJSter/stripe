d9fce3616cba4ad0fdb1bc6baed927234dc36ed1
Vivek Sahu <viveksahu1762@gmail.com>
2024-03-09 17:31:28 +0530

    Done

--- components/Login.jsx
+++ components/Login.jsx
@@ -3,29 +3,43 @@
 import React from "react";
 import { auth, provider, firestore, storage } from "../firebase/firebaseClient";
 import { signInWithPopup, GithubAuthProvider } from "firebase/auth";
-import firebase from "firebase/app";
+import { collection, doc, setDoc } from "firebase/firestore";
 
 export default function Login() {
+  const saveFirebaseUser = async (data) => {
+    try {
+      const docRef = doc(collection(firestore, "users"));
+      await setDoc(docRef, data);
+      console.log("Data saved successfully.");
+    } catch (error) {
+      console.error("Error saving data:", error);
+    }
+  };
   async function signInWithGithub() {
-    const userCredentials = await signInWithPopup(auth, provider);
+    const { user } = await signInWithPopup(auth, provider);
+    const data = {
+      email: user.email,
+      photoUrl: user.photoURL,
+      displayName: user.displayName,
+    };
+    console.log(user);
 
-    console.log({ ...userCredentials.user });
-
-    firebase
-      .firestore()
-      .collection("users")
-      .doc(userCredentials.user.uid)
-      .set({
-        uid: userCredentials.user.uid,
-        email: userCredentials.user.email,
-        name: userCredentials.user.displayName || null,
-        provider: userCredentials.user.providerData[0].providerId || null,
-        photoUrl: userCredentials.user.photoURL || null,
-        // Add additional fields as needed
-      })
-      .catch((error) => {
-        console.error("Error storing user data:", error);
-      });
+    saveFirebaseUser(data);
+    // firebase
+    //   .firestore()
+    //   .collection("users")
+    //   .doc(userCredentials.user.uid)
+    //   .set({
+    //     uid: userCredentials.user.uid,
+    //     email: userCredentials.user.email,
+    //     name: userCredentials.user.displayName || null,
+    //     provider: userCredentials.user.providerData[0].providerId || null,
+    //     photoUrl: userCredentials.user.photoURL || null,
+    //     // Add additional fields as needed
+    //   })
+    //   .catch((error) => {
+    //     console.error("Error storing user data:", error);
+    //   });
   }
 
   return (
